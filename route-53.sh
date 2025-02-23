a5fefd3787b75fe80a8f1938c9241fa56aa43cab
Sailesh Ramesh <Saileshsyrus@gmail.com>
2025-02-23 18:45:14 +0000

    feat(route-53.sh):Script for route53 failover policy

---
+++ route-53.sh
@@ -0,0 +1,40 @@
+sleep 30,
+sudo apt update,
+sudo apt install nginx -y,
+sudo apt install -y python3,
+sudo apt udpate && sudo apt install certbot python3-certbot-nginx,
+sudo mkdir -p /var/www/saileshramesh.xyz/html,
+sudo chown -R $USER:$USER /var/www/saileshramesh.xyz/html,
+sudo chmod -R 755 /var/www/saileshramesh.xyz,
+vim /var/www/saileshramesh.xyz/html/index.html,
+<html>
+    <head>
+<title> Welcome to saileshramesh.xyz!<title>
+    <head>
+    <body>
+<h1>Sucess! The saileshramesh.xyz server block is working!<h1>
+    </body>
+</html>
+
+sudo vim /etc/nginx/sites-available/saileshramesh.xyz
+server {
+    listen 80;
+    listen [::]:80;
+
+    root /var/www/saileshramesh.xyz/html;
+    index index.html index.htm index.nginx-debian.html;
+
+    server_name saileshramesh.xyz failover.saileshramesh.xyz latency.saileshramesh.xyz weighted.saileshramesh.xyz geolocation.saileshramesh.xyz
+    location / {
+    try_files furi Suri/ =404;
+    }
+}
+ 
+sudo ln -s /etc/nginx/sites-available/sailesh.ramesh.xyz /etc/nginx/sites-enabled/ sudo nginx -t
+sudo systemctl restart nginx
+sudo certbot certonly \
+--agree-tos \
+--email pinapathruni.saikiran@gmail.com \
+--manual \
+--preferred-challenges=dns \
+-d *. saileshramesh.xyz
